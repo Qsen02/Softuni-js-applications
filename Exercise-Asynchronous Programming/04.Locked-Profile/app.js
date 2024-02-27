f12e342a9cbe2706e1d95d290590345ddba4fed7
Qsen02 <diyangenchev73@gmail.com>
2024-02-27 23:00:13 +0200

    Update app.js

--- Exercise-Asynchronous Programming/04.Locked-Profile/app.js
+++ Exercise-Asynchronous Programming/04.Locked-Profile/app.js
@@ -5,40 +5,42 @@ function lockedProfile() {
             const url = "http://localhost:3030/jsonstore/advanced/profiles";
             let response = await fetch(url);
             let data = await response.json();
-            mainRef.children[0].remove();
+            console.log(data);
+            mainRef.innerHTML = "";
+            let count = 0;
             for (let id in data) {
-                let divEl = createProfile(data[id]);
+                count++;
+                let divEl = createProfile(data[id], count);
                 mainRef.appendChild(divEl);
             }
-            let curUsernames = document.querySelectorAll("#user1HiddenFields");
-            curUsernames.forEach(el => el.style.display = "none");
+            let curUsernames = document.querySelectorAll(".profile");
+            curUsernames.forEach(el => el.children[9].style.display = "none");
             let butns = document.querySelectorAll("button");
             butns.forEach(el => el.addEventListener("click", showInfo));
-            console.log(data);
         } catch (error) {
             alert("Error");
         }
     }
 
-    function createProfile(data) {
+    function createProfile(data, count) {
         let divEl = document.createElement("div");
         divEl.classList.add("profile");
         divEl.innerHTML = `<img src="./iconProfile2.png" class="userIcon" />
                            <label>Lock</label>
-                           <input type="radio" name="user1Locked" value="lock" checked>
+                           <input type="radio" name="user${count}Locked" value="lock" checked>
                            <label>Unlock</label>
-                           <input type="radio" name="user1Locked" value="unlock"><br>
+                           <input type="radio" name="user${count}Locked" value="unlock"><br>
                            <hr>
                            <label>Username</label>
-                           <input type="text" name="user1Username" value=${data.username} disabled readonly />
-                           <div id="user1HiddenFields">
+                           <input type="text" name="user${count}Username" value=${data.username} disabled readonly />
+                           <div id="user${count}HiddenFields">
                               <hr>
                               <label>Email:</label>
-                              <input type="email" name="user1Email" value="${data.email}" disabled readonly />
+                              <input type="email" name="user${count}Email" value="${data.email}" disabled readonly />
                               <label>Age:</label>
-                              <input type="text" name="user1Age" value="${data.age}" disabled readonly />
+                              <input type="email" name="user${count}Age" value="${data.age}" disabled readonly />
                            </div>
-                           <button>Show more</button>`
+                           <button>Show more</button>`;
         return divEl;
     }
 

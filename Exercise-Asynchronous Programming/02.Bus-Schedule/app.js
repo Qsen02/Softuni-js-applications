68c48156115e673fc72bf6da6f659437d9a166aa
Qsen02 <diyangenchev73@gmail.com>
2024-02-28 20:03:14 +0200

    Update app.js

--- Exercise-Asynchronous Programming/02.Bus-Schedule/app.js
+++ Exercise-Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,29 +1,44 @@
 function solve() {
     const departBtnRef = document.getElementById("depart");
     const arriveBtnREf = document.getElementById("arrive");
-    const infoRef = document.getElementById("info");
-    if (infoRef.textContent == "Not Connected") {
-        arriveBtnREf.disabled = true;
+    const infoRef = document.querySelector(".info");
+    let stopId = "depot";
+    let curStop;
+    let url = `http://localhost:3030/jsonstore/bus/schedule/`;
+    async function depart() {
+        try {
+            let res = await fetch(url + stopId);
+            if (!res.ok) {
+                throw new Error;
+            }
+            let data = await res.json();
+            departBtnRef.disabled = true;
+            arriveBtnREf.disabled = false;
+            curStop = data.name;
+            infoRef.textContent = `Next stop ${curStop}`;
+        } catch (err) {
+            infoRef.textContent = "Error";
+            departBtnRef.disabled = true;
+            arriveBtnREf.disabled = true;
+        }
     }
 
-    function depart() {
-        infoRef.children[0].remove();
-        departBtnRef.disabled = true;
-        arriveBtnREf.disabled = false;
-        let spanEl = document.createElement("span");
-        spanEl.classList.add("info");
-        spanEl.textContent = "Next stop Depot";
-        infoRef.appendChild(spanEl);
-    }
-
-    function arrive() {
-        infoRef.children[0].remove();
-        departBtnRef.disabled = false;
-        arriveBtnREf.disabled = true;
-        let spanEl = document.createElement("span");
-        spanEl.classList.add("info");
-        spanEl.textContent = "Arriving at Depot";
-        infoRef.appendChild(spanEl);
+    async function arrive() {
+        try {
+            infoRef.textContent = `Arriving at ${curStop}`;
+            let res = await fetch(url + stopId);
+            if (!res.ok) {
+                throw new Error;
+            }
+            let data = await res.json();
+            stopId = data.next;
+            departBtnRef.disabled = false;
+            arriveBtnREf.disabled = true;
+        } catch (err) {
+            infoRef.textContent = "Error";
+            departBtnRef.disabled = true;
+            arriveBtnREf.disabled = true;
+        }
     }
 
     return {
